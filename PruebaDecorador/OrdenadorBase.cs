2eae224ae10c351230fcb00949bde73d6a114d09
AnggeldDAV <anggeld.adav@gmail.com>
2024-05-20 14:44:27 +0200

    Modificado

--- PruebaDecorador/OrdenadorBase.cs
+++ PruebaDecorador/OrdenadorBase.cs
@@ -1,7 +1,9 @@
 ﻿using PruebaDecorador;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -61,6 +63,57 @@ namespace PruebaDecorador
         public override string ToString()
         {
             return $"OrdenadorSicuela, precio {this.Precio}, Ram {this.Ram}, Almacenamiento {this.Almacenamiento}";
+        }
+    }
+    //Otro decorador, es la super bomba que multiplica la potencia de fuego por 10.
+    public class DecoradorGigaRam : IOrdenable
+    { 
+    IOrdenable PC;
+    public DecoradorGigaRam(IOrdenable pc)
+        {
+            this.PC = pc;
+        }
+        public decimal Precio { get { return this.PC.Precio; } set { } }
+        public int Ram { get { return ((this.PC.Ram *10)/100)+this.PC.Ram; }set { } }
+        public int Almacenamiento { get { return this.PC.Almacenamiento; } set { } }
+        public override string ToString()
+        {
+            return $"OrdenadorGigaRam, precio {this.Precio}, Ram {this.Ram}, Almacenamiento {this.Almacenamiento}";
+        }
+    }
+    public class DecoradorOferta  : IOrdenable
+    {
+        IOrdenable PC;
+        public DecoradorOferta(IOrdenable pc)
+        {
+            this.PC = pc;
+        }
+        public decimal Precio { get 
+            {
+                var oferta = (this.PC.Precio *20) /100;
+                return this.PC.Precio - oferta; 
+            } set { } }
+
+        public int Ram { get { return this.PC.Ram; } set { } }
+        public int Almacenamiento { get { return this.PC.Almacenamiento; } set { } }
+        public override string ToString()
+        {
+            return $"Oferton, precio {this.Precio}, Ram {this.Ram}, Almacenamiento {this.Almacenamiento}";
+        }
+    }
+    public class DecoradorTera : IOrdenable
+    {
+        IOrdenable PC;
+        public DecoradorTera(IOrdenable pc)
+        {
+            this.PC = pc;
+        }
+        public decimal Precio { get { return this.PC.Precio +500; } set {} }
+        public int Ram { get { return this.PC.Ram; } set {} }
+        public int Almacenamiento { get { return this.PC.Almacenamiento + 1000; } set {} }
+        public override string ToString()
+        {
+            return $"OrdenadorTera, precio {this.Precio}, Ram {this.Ram}, Almacenamiento {this.Almacenamiento}";
         }
     }
 }
--- PruebaDecorador/Program.cs
+++ PruebaDecorador/Program.cs
@@ -6,7 +6,10 @@ IOrdenable OrdenadorAna = new OrdenadorDecoradorRamMaxima(new OrdenadorBase());
 IOrdenable OrdenadorLuisa = new OrdenadorDecoradorDiscoSolido(new OrdenadorDecoradorRamMaxima(new OrdenadorBase()));
 IOrdenable OrdenadorGeronimo = new OrdenadorDecoradorAverSiCuela(new OrdenadorDecoradorDiscoSolido(new OrdenadorBase()));
 
+IOrdenable OrdenadorTocho = new DecoradorOferta(new DecoradorTera(new DecoradorGigaRam(new OrdenadorBase())));
+
 Console.WriteLine(OrdenadorManolo);
 Console.WriteLine(OrdenadorAna);
 Console.WriteLine(OrdenadorLuisa);
 Console.WriteLine(OrdenadorGeronimo);
+Console.WriteLine(OrdenadorTocho);
