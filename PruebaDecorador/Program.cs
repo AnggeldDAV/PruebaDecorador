2c265b01a1a9e0516a5f0258cd8cb07eaf69412a
AnggeldDAV <anggeld.adav@gmail.com>
2024-05-20 13:55:12 +0200

    Modificado

--- PruebaDecorador/Program.cs
+++ PruebaDecorador/Program.cs
@@ -1,14 +1,10 @@
 ﻿
 using PruebaDecorador;
 
-IOrdenable OrdenadorBase = new OrdenadorBase();
-IOrdenable OrdenadorRam = new OrdenadorDecoradorRamMaxima(OrdenadorBase);
-IOrdenable OrdenadorDisco = new OrdenadorDecoradorDiscoSolido(OrdenadorBase);
-
-IOrdenable OrdenadorManolo = OrdenadorBase;
-IOrdenable OrdenadorAna = OrdenadorRam;
-IOrdenable OrdenadorLuisa = new OrdenadorDecoradorDiscoSolido(OrdenadorRam);
-IOrdenable OrdenadorGeronimo = new OrdenadorDecoradorAverSiCuela(OrdenadorDisco);
+IOrdenable OrdenadorManolo = new OrdenadorBase();
+IOrdenable OrdenadorAna = new OrdenadorDecoradorRamMaxima(new OrdenadorBase());
+IOrdenable OrdenadorLuisa = new OrdenadorDecoradorDiscoSolido(new OrdenadorDecoradorRamMaxima(new OrdenadorBase()));
+IOrdenable OrdenadorGeronimo = new OrdenadorDecoradorAverSiCuela(new OrdenadorDecoradorDiscoSolido(new OrdenadorBase()));
 
 Console.WriteLine(OrdenadorManolo);
 Console.WriteLine(OrdenadorAna);
